84364ec04e632a819e2eeee81d6231fe96d1dc64
Wladson Barros <barroswladson@gmail.com>
2026-04-20 17:02:53 -0300

    Criar a base dos cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,25 @@
+import Card from "@/components/card";
 import Image from "next/image";
 
 export default function Home() {
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-[radial-gradient(#e4ff6a,#98af32)] font-sans">
       <div className="flex flex-col w-[70vw] h-[90vh] gap-4">
 
-        <div className="bg-[rgb(255,255,255,0.6)] h-1/4 flex rounded-[12px]">
+        <div className="bg-[rgb(255,255,255,0.6)] h-1/4 flex rounded-[12px] p-3">
         </div>
 
-        <div className="border-2 border-white h-2/4 flex rounded-[12px]">
+        <div className="border-2 border-white h-2/4 flex rounded-[12px] p-5">
+          <Card></Card>
         </div>
 
-        <div className="bg-[rgb(255,255,255,0.6)] h-1/4 flex rounded-[12px]">
+        <div className="bg-[rgb(255,255,255,0.6)] h-1/4 flex rounded-[12px] p-3">
+          <Card></Card>
         </div>
 
       </div>
     </div>
+
   );
 }
---
+++ components/card.tsx
@@ -0,0 +1,13 @@
+export default function Card() {
+    return(
+        <div className=" aspect-9/12 h-full bg-emerald-600/80  flex flex-col items-center">
+            <div className=" w-9/10 h-1/10 bg-white/50"></div>
+
+            <div className=" w-9/10 h-5/10 bg-red-400/50"></div>
+
+            <div className=" w-9/10 h-1/10 bg-white/50"></div>
+
+            <div className=" w-9/10 h-3/10 bg-red-400/50"></div>
+        </div>
+    );
+}
