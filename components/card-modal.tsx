1034eb807abffaabd3b353deed282e4b7c94ca27
Wladson Barros <barroswladson@gmail.com>
2026-05-01 15:01:24 -0300

    Modificar o modal de local

--- components/card-modal.tsx
+++ components/card-modal.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/navigation";
 export default function CardModal (props){
     const router = useRouter();
     return(
-        <div className="w-screen h-screen bg-black/65 absolute flex items-center content-center justify-center " onClick={
+        <div className="w-screen h-screen bg-black/65 absolute flex items-center justify-end content-center justify-center p-2" onClick={
             (event)=>{
                 console.log(event.target);
                 console.log(event.currentTarget);
@@ -13,37 +13,40 @@ export default function CardModal (props){
                 }
             }
         }>
-
-            <div className="h-auto w-[50vh] bg-white text-black">
-            <form onSubmit={async (e)=>{
-                e.preventDefault();
-                const formulario =  new FormData(e.target);
-                const imagem = formulario.get("imagem");
-                if(!imagem || imagem.size === 0){
-                    props.setStatus("Arquivo não encontrado");
-                }
-                else{
-                    props.setStatus("Iniciando")
-                }
-                try{
-                const res = await fetch("http://localhost:3333/api/assets/upload",{
-                    method:"POST",
-                    body:formulario
-                });
-                if(res.ok){
-                    props.setStatus("Enviado");
-                }
-                else{
-                    props.setStatus("ERRO!");
-                };
-                }
-                catch(e){
-                    props.setStatus("ERRO!!");
-                }
+            <div className="h-full w-[50vh] bg-white text-black rounded-md">
+            <form className="" 
+                onSubmit={async (e)=>{
+                    e.preventDefault();
+                    const formulario =  new FormData(e.target);
+                    const imagem = formulario.get("imagem");
+                    if(!imagem || imagem.size === 0){
+                        props.setStatus("Arquivo não encontrado");
+                    }
+                    else{
+                        props.setStatus("Iniciando")
+                    }
+                    try{
+                    const res = await fetch("http://localhost:3333/api/assets/upload",{
+                        method:"POST",
+                        body:formulario
+                    });
+                    if(res.ok){
+                        props.setStatus("Enviado");
+                    }
+                    else{
+                        props.setStatus("ERRO!");
+                    };
+                    }
+                    catch(e){
+                        props.setStatus("ERRO!!");
+                    }
             }}>
-                <label>card</label>
-                <input type="file" name="imagem"/>
-                <input type="submit"/>
+                <label>Imagem da Carta</label>
+                <div>
+                    <label className="">Carta</label>
+                    <input className="" type="file" name="imagem"/>
+                    <input type="submit"/>
+                </div>
                 {status && <h1>{status}</h1>}
             </form>
             </div>
